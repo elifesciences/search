abb81f23cad4473967b36d0cdc107e11075fd75a
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-09-23 10:38:51 +0100

    Improved Async Assertion trait

--- tests/src/Search/AsyncAssert.php
+++ tests/src/Search/AsyncAssert.php
@@ -2,19 +2,63 @@
 
 namespace tests\eLife\Search;
 
-use Closure;
-use LogicException;
+use BadMethodCallException;
 use function GuzzleHttp\Promise\all;
 
+/**
+ * @method asyncAssertEquals($expected, $actual, $message = "")
+ * @method asyncAssertArrayHasKey()
+ * @method asyncAssertClassHasAttribute()
+ * @method asyncAssertArraySubset()
+ * @method asyncAssertClassHasStaticAttribute()
+ * @method asyncAssertContains()
+ * @method asyncAssertContainsOnly()
+ * @method asyncAssertContainsOnlyInstancesOf()
+ * @method asyncAssertCount()
+ * @method asyncAssertEmpty()
+ * @method asyncAssertEqualXMLStructure()
+ * @method asyncAssertFalse()
+ * @method asyncAssertFileEquals()
+ * @method asyncAssertFileExists()
+ * @method asyncAssertGreaterThan()
+ * @method asyncAssertGreaterThanOrEqual()
+ * @method asyncAssertInfinite()
+ * @method asyncAssertInstanceOf()
+ * @method asyncAssertInternalType()
+ * @method asyncAssertJsonFileEqualsJsonFile()
+ * @method asyncAssertJsonStringEqualsJsonFile()
+ * @method asyncAssertJsonStringEqualsJsonString()
+ * @method asyncAssertLessThan()
+ * @method asyncAssertLessThanOrEqual()
+ * @method asyncAssertNan()
+ * @method asyncAssertNull()
+ * @method asyncAssertObjectHasAttribute()
+ * @method asyncAssertRegExp()
+ * @method asyncAssertStringMatchesFormat()
+ * @method asyncAssertStringMatchesFormatFile()
+ * @method asyncAssertSame()
+ * @method asyncAssertStringEndsWith()
+ * @method asyncAssertStringEqualsFile()
+ * @method asyncAssertStringStartsWith()
+ * @method asyncAssertThat()
+ * @method asyncAssertTrue()
+ * @method asyncAssertXmlFileEqualsXmlFile()
+ * @method asyncAssertXmlStringEqualsXmlFile()
+ * @method asyncAssertXmlStringEqualsXmlString()
+ */
 trait AsyncAssert
 {
-    public function asyncAssertEqual($expected, $actual)
+    public function __call($name, $arguments)
     {
-        if (!method_exists($this, 'assertEquals')) {
-            throw new LogicException('This should only be run from test cases.');
+        if (strpos($name, 'async') === 0) {
+            // Trim off the async.
+            $fn = lcfirst(substr($name, 5));
+            if (method_exists($this, $fn)) {
+                // Wait for arguments.
+                all($arguments)->then([$this, $fn]);
+            } else {
+                throw new BadMethodCallException('Async assert method not found', get_class($this), $fn);
+            }
         }
-        all([$expected, $actual])->then(Closure::bind(function ($expected, $actual) {
-            $this->assertEquals($expected, $actual);
-        }, $this));
     }
 }
