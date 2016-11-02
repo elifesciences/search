fa212b79bb728c7a325d1ac0e98ae10d00fd9741
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-11-02 17:24:06 +0000

    Added first mapping files based on current test data

---
+++ src/Search/Api/Elasticsearch/Command/BuildIndexCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace eLife\Search\Api\Elasticsearch\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+class BuildIndexCommand extends Command
+{
+    // Waiting for completion of indexes.
+}
