c71f30008ef258c6879769b99bf838ebad924563
Giorgio Sironi <g.sironi@elifesciences.org>
2016-12-12 14:26:37 +0000

    Stable smoke test
    This uses api-gateway and journal-cms behind it (at least), but should be pretty stable:
    - in `ci`, with api-dummy
    - in `end2end`, with the other end2end machines (just tried it)
    - in `prod`, with the other prod machines

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -2,3 +2,4 @@
 set -ex
 
 [ $(curl --write-out %{http_code} --silent --output /dev/null localhost/ping) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null localhost/search?for=notexistentkeywordthatwouldmakeanemptyresult) == 200 ]
