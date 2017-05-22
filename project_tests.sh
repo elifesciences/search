1a76b99d9f0c5544821487bcfa93305cd04080c5
Giorgio Sironi <g.sironi@elifesciences.org>
2017-05-22 11:01:31 +0000

    project_tests.sh allows an environment

--- project_tests.sh
+++ project_tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+env="${1:-ci}"
+
 rm -f build/*.xml
 proofreader src/ tests/ web/
 vendor/bin/phpunit --log-junit build/phpunit.xml
-bin/ci-import ci
+bin/ci-import "$env"
