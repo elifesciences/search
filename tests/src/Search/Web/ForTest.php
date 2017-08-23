70c78beec6b440af749fc6ef1140604b68892ba5
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-08-23 09:35:56 +0100

    Add negative test

--- tests/src/Search/Web/ForTest.php
+++ tests/src/Search/Web/ForTest.php
@@ -24,6 +24,21 @@ final class ForTest extends ElasticTestCase
         $this->assertEquals($response->items[1]->id, '15276');
     }
 
+    public function test_all_for_words_have_to_match()
+    {
+        $this->addDocumentsToElasticSearch([
+            $this->getArticleFixture(0),
+            $this->getArticleFixture(1),
+            $this->getArticleFixture(2),
+            $this->getArticleFixture(3),
+        ]);
+
+        $this->newClient();
+        $this->jsonRequest('GET', '/search', ['for' => 'Robert Jones']);
+        $response = $this->getJsonResponse();
+        $this->assertEquals($response->total, 0);
+    }
+
     public function test_all_for_words_fuzzy_match()
     {
         $this->addDocumentsToElasticSearch([
