c62da2deb1a138812b3fc612752987bef97fe3a8
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-14 14:32:56 +0000

    Merging main and environment-specific config file

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,9 +6,11 @@ $defaultFile = __DIR__.'/../config.php';
 $environmentSpecificFile = __DIR__.'/../config/'.getenv('ENVIRONMENT_NAME').'.php';
 if (file_exists($defaultFile)) {
     $config = include $defaultFile;
-} elseif (file_exists($environmentSpecificFile)) {
-    // temporary fallback to environment-based file
-    $config = include $environmentSpecificFile;
 } else {
-    throw new RuntimeException("Can't find a config/config.php file");
+    $config = [];
+}
+
+if (file_exists($environmentSpecificFile)) {
+    // temporary fallback to environment-based file
+    $config = array_merge($config, include $environmentSpecificFile);
 }
