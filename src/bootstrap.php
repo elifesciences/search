d9b0b4c46f6f62a2f3f1dff84344437dbd868150
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-14 14:31:13 +0000

    Moving location of main config file

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,13 +2,13 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 umask(0002);
-$defaultFile = __DIR__.'/../config/config.php';
-$legacyFile = __DIR__.'/../config/'.getenv('ENVIRONMENT_NAME').'.php';
+$defaultFile = __DIR__.'/../config.php';
+$environmentSpecificFile = __DIR__.'/../config/'.getenv('ENVIRONMENT_NAME').'.php';
 if (file_exists($defaultFile)) {
     $config = include $defaultFile;
-} elseif (file_exists($legacyFile)) {
+} elseif (file_exists($environmentSpecificFile)) {
     // temporary fallback to environment-based file
-    $config = include $legacyFile;
+    $config = include $environmentSpecificFile;
 } else {
     throw new RuntimeException("Can't find a config/config.php file");
 }
