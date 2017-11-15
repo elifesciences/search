136cc3a33c9b13212ac2ae9798447abea871936e
Giorgio Sironi <info@giorgiosironi.com>
2017-11-15 15:40:39 +0000

    Add support for a config.php file (#167)
    * Add support for a config.php file
    
    * Remove mandatory --env, as it's not read
    
    * Moved bootstrap.php in src/
    
    * Moving location of main config file
    
    * Merging main and environment-specific config file

--- web/app_ci.php
+++ web/app_ci.php
@@ -1,10 +1,8 @@
 <?php
 
-require_once __DIR__.'/bootstrap.php';
+require_once __DIR__.'/../src/bootstrap.php';
 
 use eLife\Search\Kernel;
-
-$config = include __DIR__.'/../config/ci.php';
 
 $kernel = new Kernel($config);
 
--- web/app_continuumtest.php
+++ web/app_continuumtest.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__.'/bootstrap.php';
-
-$config = include __DIR__.'/../config/continuumtest.php';
+require_once __DIR__.'/../src/bootstrap.php';
 
 (new eLife\Search\Kernel($config))->run();
