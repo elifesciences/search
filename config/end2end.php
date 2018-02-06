0ae059429ff092ad562ceb3df573f98d9af50a61
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-06 16:10:07 +0000

    Turn on ElasticSearch client logging, to obtain the duration of requests

--- config/end2end.php
+++ config/end2end.php
@@ -5,6 +5,7 @@ return [
     'elastic_servers' => ['http://end2end--search--1.elife.internal:9200'],
     'api_url' => 'http://end2end--gateway.elife.internal/',
     'api_requests_batch' => 20,
+    'elastic_logging' => true,
     'aws' => [
         'queue_name' => 'search--end2end',
         'credential_file' => true,
