db9a35fa4d2eff4b114e8630079df7d9e2b8d402
Giorgio Sironi <info@giorgiosironi.com>
2018-03-12 15:55:29 +0000

    Retry on 504 in smoke tests (#200)
    * Retry on 504 in smoke tests
    
    Akin to what we do for journal's /status, ElasticSearch often times out on the first requests after startup.
    
    * Make executable

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -2,6 +2,4 @@
 set -ex
 
 [ $(curl --write-out %{http_code} --silent --output /dev/null localhost/ping) == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null localhost/search?for=notexistentkeywordthatwouldmakeanemptyresult) == 200 ]
-# output should have a "types" field
-[ $(curl --silent localhost/search?for=something | jq 'has("types")') == "true" ]
+retry ./smoke_tests_elasticsearch.sh 3
