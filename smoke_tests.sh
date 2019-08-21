6c718f583284a1ee1bed926ecaeac363c89fdd77
Luke <lsh-0@users.noreply.github.com>
2019-08-21 13:39:38 +0930

    added temporary delay before running smoke tests to get search-formula build working

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -4,5 +4,8 @@ set -ex
 attempts=3
 delay=10
 
+# temporary, to get 18.04 formula branch passing. remove once upgraded. this delay is incorporated there
+sleep 20 
+
 retry ./smoke_tests_app.sh $attempts $delay
 retry ./smoke_tests_elasticsearch.sh $attempts $delay
