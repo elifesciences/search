2e845e3fc3a5bc49453ce69a38f9e10c61efb76f
Luke <lsh-0@users.noreply.github.com>
2019-09-02 15:13:35 +0930

    adds an exception to calling the smoke tests removed extravagent delay before calling script

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
+hostname | grep 'fresh-follower-node' && {
+    echo "This is a follower node without an elasticsearch leader. Smoke tests cannot be run."
+    exit 0
+}
+
 attempts=3
 delay=10
-
-# temporary, to get 18.04 formula branch passing. remove once upgraded. this delay is incorporated there
-sleep 20 
 
 retry ./smoke_tests_app.sh $attempts $delay
 retry ./smoke_tests_elasticsearch.sh $attempts $delay
