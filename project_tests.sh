35d5b8f11acbad8685b1e67c9dcb31d03e377c6b
Luke <lsh-0@users.noreply.github.com>
2022-08-17 14:58:49 +0930

    project_tests, adds a kludge to bump the max open file limit while testing. file limit is reset on script exit.

--- project_tests.sh
+++ project_tests.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+# lsh@2022-08-17: some change has maxed out the default (soft) file limit of 1024.
+# this is a kludge and the real culprit should be found and fixed.
+# - https://github.com/elifesciences/search/pull/382
+ulimit -S -n 2048
+
+function cleanup {
+    ulimit -S n 1024
+}
+trap cleanup EXIT
+
 rm -f build/*.xml
 vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p src/ tests/ web/
 vendor/bin/phpunit --log-junit build/phpunit.xml
