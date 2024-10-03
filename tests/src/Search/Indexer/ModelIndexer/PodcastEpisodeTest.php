56f07dd1da57fcdd0d5c68fdd49ee2902c545fca
Scott Aubrey <scott@aubrey.org.uk>
2024-10-03 14:48:35 +0100

    add PodcastEpisodeIndexer and test

---
+++ tests/src/Search/Indexer/ModelIndexer/PodcastEpisodeTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace tests\eLife\Search\Indexer\ModelIndexer;
+
+use PHPUnit_Framework_TestCase;
+use eLife\ApiSdk\Model\PodcastEpisode;
+use eLife\Search\Indexer\ModelIndexer\PodcastEpisodeIndexer;
+
+
+final class PodcastEpisodeIndexerTest extends PHPUnit_Framework_TestCase
+{
+    use GetSerializer;
+    use ModelProvider;
+
+    /**
+     * @var PodcastEpisodeIndexer
+     */
+    private $indexer;
+
+    protected function setUp()
+    {
+        $this->indexer = new PodcastEpisodeIndexer($this->getSerializer());
+    }
+
+    protected function getModel() : string
+    {
+        return 'podcast-episode';
+    }
+
+    protected function getModelClass() : string
+    {
+        return PodcastEpisode::class;
+    }
+
+    protected function getVersion() : int
+    {
+        return 1;
+    }
+
+    /**
+     * @dataProvider modelProvider
+     * @test
+     */
+    public function testIndexOfPodcastEpisode(PodcastEpisode $podcastEpisode)
+    {
+        $changeSet = $this->indexer->prepareChangeSet($podcastEpisode);
+
+        $this->assertCount(0, $changeSet->getDeletes());
+        $this->assertCount(1, $changeSet->getInserts());
+
+        $insert = $changeSet->getInserts()[0];
+        $article = $insert['json'];
+        $id = $insert['id'];
+        $this->assertJson($article, 'PodcastEpisode is not valid JSON');
+        $this->assertNotNull($id, 'An ID is required.');
+        $this->assertStringStartsWith('podcast-episode-', $id, 'ID should be assigned an appropriate prefix.');
+    }
+}
