2365183421dab7b3a12793b6e91d60133bbdb3ee
Scott Aubrey <scott@aubrey.org.uk>
2024-10-03 16:24:52 +0100

    add ReviewedPreprintIndexer and test

---
+++ src/Search/Indexer/ModelIndexer/ReviewedPreprintIndexer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace eLife\Search\Indexer\ModelIndexer;
+
+use eLife\ApiSdk\Model\ReviewedPreprint;
+use eLife\ApiSdk\Model\Model;
+use eLife\Search\Api\Elasticsearch\MappedElasticsearchClient;
+use eLife\Search\Indexer\ChangeSet;
+use Symfony\Component\Serializer\Serializer;
+
+final class ReviewedPreprintIndexer extends AbstractModelIndexer
+{
+    private MappedElasticsearchClient $client;
+
+    public function __construct(Serializer $serializer, MappedElasticsearchClient $client)
+    {
+        parent::__construct($serializer);
+        $this->client = $client;
+    }
+
+    protected function getSdkClass(): string
+    {
+        return ReviewedPreprint::class;
+    }
+
+    /**
+     * @param ReviewedPreprint $reviewedPreprint
+     * @return ChangeSet
+     */
+    public function prepareChangeSet(Model $reviewedPreprint) : ChangeSet
+    {
+        $changeSet = new ChangeSet();
+
+        // Don't index if article with same id present in index.
+        foreach ([
+            'research-article',
+            'tools-resources',
+            'short-report',
+            'research-advance',
+        ] as $type) {
+            if ($this->client->getDocumentById($type.'-'. $reviewedPreprint->getId(), null, true) !== null) {
+                return $changeSet;
+            }
+        }
+
+        $reviewedPreprintObject = json_decode($this->serialize($reviewedPreprint));
+        $reviewedPreprintObject->type = 'reviewed-preprint';
+        $reviewedPreprintObject->body = $reviewedPreprint->getIndexContent() ?? '';
+        $reviewedPreprintObject->snippet = ['format' => 'json', 'value' => json_encode($this->snippet($reviewedPreprint))];
+
+        $this->addSortDate($reviewedPreprintObject, $reviewedPreprint->getStatusDate());
+
+
+        $changeSet->addInsert(
+            $reviewedPreprintObject->type.'-'.$reviewedPreprint->getId(),
+            json_encode($reviewedPreprintObject),
+        );
+        return $changeSet;
+    }
+}
---
+++ tests/src/Search/Indexer/ModelIndexer/ReviewedPreprintIndexerTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace tests\eLife\Search\Indexer\ModelIndexer;
+
+use eLife\ApiSdk\Model\ReviewedPreprint;
+use PHPUnit_Framework_TestCase;
+use eLife\Search\Api\Elasticsearch\MappedElasticsearchClient;
+use eLife\Search\Indexer\ModelIndexer\ReviewedPreprintIndexer;
+use Mockery;
+
+final class ReviewedPreprintIndexerTest extends PHPUnit_Framework_TestCase
+{
+    use GetSerializer;
+    use ModelProvider;
+
+    /**
+     * @var MockInterface
+     */
+    private $elastic;
+
+    /**
+     * @var ReviewedPreprintIndexer
+     */
+    private $indexer;
+
+    protected function setUp()
+    {
+        $this->elastic = Mockery::mock(MappedElasticsearchClient::class);
+        $this->indexer = new ReviewedPreprintIndexer($this->getSerializer(), $this->elastic);
+    }
+
+    protected function getModelDefinitions()
+    {
+        return [
+            ['model' => 'reviewed-preprint', 'modelClass' => ReviewedPreprint::class, 'version' => 1]
+        ];
+    }
+
+    /**
+     * @dataProvider modelProvider
+     * @test
+     */
+    public function testIndexOfPodcastEpisode(ReviewedPreprint $reviewedPreprint)
+    {
+        $this->elastic->shouldReceive('getDocumentById')
+            ->with('research-article-'.$reviewedPreprint->getId(), null, true)
+            ->andReturn(null);
+        $this->elastic->shouldReceive('getDocumentById')
+            ->with('tools-resources-'.$reviewedPreprint->getId(), null, true)
+            ->andReturn(null);
+        $this->elastic->shouldReceive('getDocumentById')
+            ->with('short-report-'.$reviewedPreprint->getId(), null, true)
+            ->andReturn(null);
+        $this->elastic->shouldReceive('getDocumentById')
+            ->with('research-advance-'.$reviewedPreprint->getId(), null, true)
+            ->andReturn(null);
+        $changeSet = $this->indexer->prepareChangeSet($reviewedPreprint);
+
+        $this->assertCount(0, $changeSet->getDeletes());
+        $this->assertCount(1, $changeSet->getInserts());
+
+        $insert = $changeSet->getInserts()[0];
+        $article = $insert['json'];
+        $id = $insert['id'];
+        $this->assertJson($article, 'Article is not valid JSON');
+        $this->assertNotNull($id, 'An ID is required.');
+        $this->assertStringStartsWith('reviewed-preprint-', $id, 'ID should be assigned an appropriate prefix.');
+    }
+
+
+    /**
+     * @dataProvider modelProvider
+     * @test
+     */
+    public function testIndexOfReviewedPreprintSkipped(ReviewedPreprint $reviewedPreprint)
+    {
+        $this->elastic->shouldReceive('getDocumentById')
+            ->with('research-article-'.$reviewedPreprint->getId(), null, true)
+            ->andReturn('found');
+
+        $changeSet = $this->indexer->prepareChangeSet($reviewedPreprint);
+
+        $this->assertCount(0, $changeSet->getDeletes());
+        $this->assertCount(0, $changeSet->getInserts());
+    }
+
+    /**
+     * @dataProvider modelProvider
+     * @test
+     */
+    public function testIndexOfReviewedPreprintSkippedToolsResources(ReviewedPreprint $reviewedPreprint)
+    {
+        $this->elastic->shouldReceive('getDocumentById')
+            ->with('research-article-'.$reviewedPreprint->getId(), null, true)
+            ->andReturn(null);
+        $this->elastic->shouldReceive('getDocumentById')
+            ->with('tools-resources-'.$reviewedPreprint->getId(), null, true)
+            ->andReturn('found');
+
+        $changeSet = $this->indexer->prepareChangeSet($reviewedPreprint);
+
+        $this->assertCount(0, $changeSet->getDeletes());
+        $this->assertCount(0, $changeSet->getInserts());
+    }
+}
