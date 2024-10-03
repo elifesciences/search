11849a92c5ce50ea6e1f0b728b732ea6f1d58a1e
Scott Aubrey <scott@aubrey.org.uk>
2024-10-03 14:43:55 +0100

    add LabsPostIndexer and test

---
+++ tests/src/Search/Indexer/ModelIndexer/LabsPostWorkflowTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace tests\eLife\Search\Indexer\ModelIndexer;
+
+use PHPUnit_Framework_TestCase;
+use eLife\ApiSdk\Model\LabsPost;
+use eLife\Search\Indexer\ModelIndexer\LabsPostIndexer;
+
+
+final class LabsPostIndexerTest extends PHPUnit_Framework_TestCase
+{
+    use GetSerializer;
+    use ModelProvider;
+
+    /**
+     * @var LabsPostIndexer
+     */
+    private $indexer;
+
+    protected function setUp()
+    {
+        $this->indexer = new LabsPostIndexer($this->getSerializer());
+    }
+
+    protected function getModel() : string
+    {
+        return 'labs-post';
+    }
+
+    protected function getModelClass() : string
+    {
+        return LabsPost::class;
+    }
+
+    protected function getVersion() : int
+    {
+        return 1;
+    }
+
+    /**
+     * @dataProvider modelProvider
+     * @test
+     */
+    public function testIndexOfLabsPost(LabsPost $labsPost)
+    {
+        $changeSet = $this->indexer->prepareChangeSet($labsPost);
+
+        $this->assertCount(0, $changeSet->getDeletes());
+        $this->assertCount(1, $changeSet->getInserts());
+
+        $insert = $changeSet->getInserts()[0];
+        $article = $insert['json'];
+        $id = $insert['id'];
+        $this->assertJson($article, 'LabsPost is not valid JSON');
+        $this->assertNotNull($id, 'An ID is required.');
+        $this->assertStringStartsWith('labs-post-', $id, 'ID should be assigned an appropriate prefix.');
+    }
+}
