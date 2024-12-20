46eef0b694b188f7ebfce7b10f6ab5455bf970e4
Scott Aubrey <scott@aubrey.org.uk>
2024-12-20 15:46:21 +0000

    add type to property

--- src/Search/Api/Elasticsearch/ResponsePartials/HitItem.php
+++ src/Search/Api/Elasticsearch/ResponsePartials/HitItem.php
@@ -12,8 +12,9 @@ final class HitItem
      * @Type("array")
      * @SerializedName("_source")
      * @Accessor(setter="setSource")
+     * @var array<string, mixed> $_source
      */
-    public $_source;
+    public array $_source;
 
     public function setSource(array $_source)
     {
