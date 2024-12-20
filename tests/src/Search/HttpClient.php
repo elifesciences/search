792d7ebdae7043c6b3b7826105afba2839df55ec
Scott Aubrey <scott@aubrey.org.uk>
2024-12-20 16:22:41 +0000

    add types to property

--- tests/src/Search/HttpClient.php
+++ tests/src/Search/HttpClient.php
@@ -4,12 +4,13 @@ namespace tests\eLife\Search;
 
 use eLife\ApiClient\HttpClient\Guzzle6HttpClient;
 use GuzzleHttp\Client;
+use eLife\ApiClient\HttpClient as ApiHttpClient;
 
 trait HttpClient
 {
-    public static $httpClient;
+    public static ApiHttpClient $httpClient;
 
-    final protected static function getHttpClient() : \eLife\ApiClient\HttpClient
+    final protected static function getHttpClient() : ApiHttpClient
     {
         if (null === static::$httpClient) {
             static::$httpClient = new Guzzle6HttpClient(new Client([
