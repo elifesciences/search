2514feb221a07a6429fefd247b349391f84418ef
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-01-15 15:27:17 +0000

    Try removing obsolete cludge that was probably needed for the old CI system

--- slow-tests.sh
+++ slow-tests.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# lsh@2022-08-17: some change has maxed out the default (soft) file limit of 1024.
-# this is a kludge and the real culprit should be found and fixed.
-# - https://github.com/elifesciences/search/pull/382
-ulimit -S -n 2048
-
-function cleanup {
-    rc=$?
-    ulimit -S -n 1024
-    exit $rc
-}
-trap cleanup EXIT
-
 echo "Creating, deleting an index"
 bin/ci-lifecycle
 
