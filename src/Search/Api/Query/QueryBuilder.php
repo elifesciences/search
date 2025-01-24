635856593a6b966242faa79ba28884168f472f86
Nathan Lisgo <nathan@lisgo.co.uk>
2025-01-24 23:36:41 +0000

    Remove trailing whitespace from betweenDates method declaration

--- src/Search/Api/Query/QueryBuilder.php
+++ src/Search/Api/Query/QueryBuilder.php
@@ -24,7 +24,7 @@ interface QueryBuilder
 
     public function whereTerms(string $strength = null, string $significance = null, bool $prcOnly = true) : QueryBuilder;
 
-    public function betweenDates(DateTimeImmutable $startDate = null, DateTimeImmutable $endDate = null) : QueryBuilder;    
+    public function betweenDates(DateTimeImmutable $startDate = null, DateTimeImmutable $endDate = null) : QueryBuilder;
 
     public function getRawQuery() : array;
 }
