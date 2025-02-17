22eb3e76e44185e178c02a6f75bfe452a25fee9b
Scott Aubrey <scott@aubrey.org.uk>
2025-02-17 11:56:24 +0000

    remove redundant smokes tests as part of reindex
    https://github.com/elifesciences/issues/issues/9175

--- smoke_tests_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-
-test1=$(curl --write-out '%{http_code}' --silent --output /dev/null localhost/ping)
-[[ "$test1" == "200" ]]
--- smoke_tests_opensearch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-test1=$(curl --write-out '%{http_code}' --silent --output /dev/null localhost/search?for=notexistentkeywordthatwouldmakeanemptyresult)
-[[ "$test1" == "200" ]]
-
-# output should have a "types" field
-test2=$(curl --silent localhost/search?for=something | jq 'has("types")')
-[[ "$test2" == "true" ]]
