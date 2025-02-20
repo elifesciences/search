28bb24c03b827b772caf688883b9718dea3047d4
David Moulton <d.moulton@elifesciences.org>
2025-02-20 15:47:28 +0000

    Express intention with an incomplete test
    https://github.com/elifesciences/issues/issues/9175

---
+++ tests/src/Search/Web/ElifeAssessmentTermsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace src\Search\Web;
+
+use PHPUnit\Framework\Attributes\Group;
+use tests\eLife\Search\Web\ElasticTestCase;
+
+#[Group('web')]
+class ElifeAssessmentTermsTest extends ElasticTestCase
+{
+    public function testSignificanceFilteringWorks()
+    {
+        $this->markTestIncomplete();
+    }
+}
